e788ddc43d95121c4bc8e73f2de590a9148e4e0b
Michael Cohen <miccoh1994@gmail.com>
2022-05-02 12:23:17 +0200

    fix url

--- app/db.server.ts
+++ app/db.server.ts
@@ -24,11 +24,8 @@ function getClient() {
   const { DATABASE_URL } = process.env;
   invariant(typeof DATABASE_URL === "string", "DATABASE_URL env var not set");
 
-  const databaseUrl = new URL(DATABASE_URL);
 
-  const isLocalHost = databaseUrl.hostname === "localhost";
-
-  console.log(`🔌 setting up prisma client to ${databaseUrl.host}`);
+  console.log(`🔌 setting up prisma client to `);
   // NOTE: during development if you change anything in this function, remember
   // that this only runs once per server restart and won't automatically be
   // re-run per request like everything else is. So if you need to change
@@ -36,7 +33,7 @@ function getClient() {
   const client = new PrismaClient({
     datasources: {
       db: {
-        url: databaseUrl.toString(),
+        url: DATABASE_URL,
       },
     },
   });
